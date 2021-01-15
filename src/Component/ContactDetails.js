15456200c4af4eaa7fec79b720de7600e6e84732
Patrick Horn <pat.horn90@gmail.com>
2021-01-14 22:02:54 -0500

    back button implementation complete, awaiting feedback from project manager

--- src/Component/ContactDetails.js
+++ src/Component/ContactDetails.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(() => ({
   detailsContainer: {
@@ -12,9 +14,17 @@ const useStyles = makeStyles(() => ({
 const ContactDetails = (contacts) => {
   const classes = useStyles();
   let { uuid } = useParams();
+  let history = useHistory();
   return (
     <div className={classes.detailsContainer}>
       <h1>Contact Details Page for User: {uuid}</h1>
+      <Button
+        onClick={() => history.goBack()}
+        variant="contained"
+        color="primary"
+      >
+        Back
+      </Button>
     </div>
   );
 };
